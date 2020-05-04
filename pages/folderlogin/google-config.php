f289c473d47163a7238d8351828cc5a9ec27e6a9
RobertPlaiasu <59140274+RobertPlaiasu@users.noreply.github.com>
2020-05-04 20:18:47 +0300

    Delete google-config.php

--- pages/folderlogin/google-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require 'google-login/vendor/autoload.php';
-
-  $client=new Google_Client();
-  $client->setClientid('40981501409-bloe19393h167tl7q81e556p34u470i2.apps.googleusercontent.com');
-  $client->setClientSecret('jm1cJH1cbF9vKuoKNqPAxZU-');
-  $client->setRedirectUri('http://stoicescu.eu/pages/folderlogin/login-google.php');
-  $client->addScope('email');
-  $client->addScope('profile');
