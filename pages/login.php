dd8b4832e7e1f0c744e272a23a348c0cd1c5895c
GrigoAleex <grigorescu.aleex@gmail.com>
2020-05-22 21:46:21 +0300

    New Login

--- pages/login.php
+++ pages/login.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['mailUser']) || isset($_SESSION['mailGmail'])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en" style="height:100%">
+<html lang="en">
 
     <head>
         <meta charset="UTF-8">
@@ -18,12 +18,59 @@ if (isset($_SESSION['mailUser']) || isset($_SESSION['mailGmail'])) {
         <title>Stoodle | Conectare</title>
         <link rel="icon" href="../logo.ico" type="image/x-icon" />
         <link rel="stylesheet" href="./CSS/login.css">
-        <link rel="stylesheet" href="./CSS/base.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     </head>
 
-    <body style=" height:100%!important">
-        <div class="row" style="overflow: scroll;-webkit-overflow-scrolling: touch; height:100%!important">
+    <body>
+        <div class="container">
+            <div id="headline">
+                <img src="./Images/logo.png" alt="Logo">
+                <h1>Autentificare</h1>
+            </div>
+            <form action="folderlogin/loginphp.php" method="post">
+                <div class="form-group">
+                    <?php
+                        getSecondErrorMessage("sqlierror","Eroare server!");
+                        getSecondErrorMessage("fatalerrorsql","Eroare aplicatie!");
+                        getSuccesMessage("resetare","Parola a fost schimbata");
+                        getSuccesMessage("register","Verifica adresa de email pentru a te putea loga!");
+                    ?>
+                    <input type="email" class="form-control" placeholder=" " id="email" />
+                    <label for="email">Email</label>
+                </div>
+                <small class="form-text alert-note">
+                    <?php
+                        getMainErrorMessage("emptymail",'Completeaza campul !');
+                        getMainErrorMessage("nuUser",'Email-ul nu a fot gasit!');
+                        getMainErrorMessage("invalidmailuserid",'Email-ul este invalid!');
+                    ?>
+                </small>
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder=" " id="password" />
+                    <label for="password">Parola</label>
+                       
+                </div>
+                <small class="form-text alert-note">
+                    <?php
+                        getMainErrorMessage("emptypass","Completeaza campul");
+                        getMainErrorMessage("invalidpassw","Pentru parola se folosesc doar caractere a alfabetui englez si cifrele de la 0-9!");
+                        getMainErrorMessage("parolagresita","Combinatia email si parola este gresita");
+                    ?>
+                </small> 
+                <a href="./register.php">Creeaza-ti un cont!</a>
+
+                <a class="btn btn-outline-dark" 
+                    <?php echo 'href="'.$client->createAuthUrl().'"' ?>  
+                    role="button" style="text-transform:none; width: 100%; padding: 1em; margin: .3em 0;">
+                    <img width="20px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" 
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
+                    Login with Google
+                </a>
+                
+                <input type="submit" name="loginsubmit" class="button" />
+            </form>
+        </div>
+        <!-- <div class="row">
 
             <div class="col-lg-7">
                 <div id="text">
@@ -111,7 +158,7 @@ if (isset($_SESSION['mailUser']) || isset($_SESSION['mailGmail'])) {
 
                 </div>
             </div>
-        </div>
+        </div> -->
     </body>
 
 </html>
